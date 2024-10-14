e71f57b3b46f323b49061ecd5cc24a14ad76973b
DEVANSHU PATIL <152208944+patildevanshu@users.noreply.github.com>
2024-10-14 20:20:30 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
 
+// app=express();
 const app = express();
 app.use(cors());
 app.use(express.json());
